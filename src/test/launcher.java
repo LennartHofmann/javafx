6a40193c9cb9cf72ebb27a6469927df906d29e21
LennartHofmann <33035508+LennartHofmann@users.noreply.github.com>
2018-10-23 08:40:54 +0200

    first javafx

--- src/test/launcher.java
+++ src/test/launcher.java
@@ -1,9 +1,32 @@
 package test;
 
-public class launcher {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class launcher extends Application{
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		launch();
 	}
-	
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		VBox parent = new VBox();
+		
+		TextField tf = new TextField();
+		tf.setText("Hello World!");
+		parent.getChildren().add(tf);
+		
+		
+		Scene szene = new Scene(parent);
+		primaryStage.setScene(szene);
+		primaryStage.show();
+		
+		
+		
+	}
 }
